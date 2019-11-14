e5b149ff6c105a2f9f98154eca6e16bdb849ce25
William <williamong1400@gmail.com>
2019-11-14 20:57:11 +0700

    Operator

---
+++ operator.pl
@@ -0,0 +1,29 @@
+:- use_module(library(dcg/basics)).
+
+op_num -->
+	"+"; "-"; "("; ")"; "*"; "%"; "/"; "**"; "//".
+
+op_compare -->
+	"=="; "!="; "<>"; ">"; "<"; ">="; "<=".
+
+op_assignment -->
+	"="; "+="; "-="; "*="; "/="; "//="; "%="; "**=".
+
+op_bitwise -->
+	"&";"|";"^";"~";"<<";">>".
+
+op_logical -->
+	"and";"or";"not".
+
+op_membership -->
+	"in"; "not in".
+
+op_identity -->
+	"is"; "is not".
+
+op -->
+	op_num;op_compare;op_assignment;op_bitwise;op_logical;op_membership;op_identity.
+
+
+
+
