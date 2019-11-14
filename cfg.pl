a3824f8f68e5a7b3303e73c2ff0c1747df9b3ba0
William <williamong1400@gmail.com>
2019-11-14 21:43:26 +0700

    Pika

--- cfg.pl
+++ cfg.pl
@@ -2,6 +2,13 @@
 :- use_module(library(pio)).
 :- include(number).
 :- include(alphabet).
+:- include(operator).
+:- initialization(write('======='),program).
+:- initialization(nl,program).
+:- initialization(write('Start!!'),program).
+:- initialization(nl,program).
+:- initialization(write('======='),program).
+:- initialization(nl,program).
 
 start(X) :-
 	phrase_from_file(X, 'input1.txt').
---
+++ keyword.pl
@@ -0,0 +1,18 @@
+loop -->
+	"for"; "while"; "range"; "in"; ":"; "break";"continue".
+
+class -->
+	"class".
+
+condition -->
+	"if"; "elif"; "else"; "pass";":";"switch";"case".
+
+%% method -->
+%% 	(".",
+
+kwd -->
+	"print";"with";"import";"from";"as";"raise";"with".
+
+function -->
+	"def";":";"return".
+
