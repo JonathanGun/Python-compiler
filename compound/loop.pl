76fcfaf33ba6fd34c926fd4276c5935895a21fcd
William <williamong1400@gmail.com>
2019-11-15 21:59:20 +0700

    fasfas

--- compound/loop.pl
+++ compound/loop.pl
@@ -1,10 +1,19 @@
 while -->
-	"while",blanks, expr,blanks, ":",block.
+	"while" , blanks , expr , blanks , ":" , block.
 
 for -->
-    "for",blanks,variable,blanks,"in",blanks,(expr;function),":",block.
+    "for" , blanks , variable , blanks , "in" , blanks , (expr;function) ,":" , block.
+
 break -->
+	{
+		current_indent(X),
+		X > 0
+	},
 	"break".
 
 continue -->
+	{
+		current_indent(X),
+		X > 0
+	},
 	"continue".
