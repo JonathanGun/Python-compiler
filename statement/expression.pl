16bd34012a31a8700f08a0d440675b2d2b4cefb4
Jonathan Yudi <jonathanyudigun@gmail.com>
2019-11-15 20:50:46 +0700

    FIX EXPRESSION BITCH

--- statement/expression.pl
+++ statement/expression.pl
@@ -1,23 +1,32 @@
-expr_prefix -->
-	eps; (op_prefix, any_blanks).
-
 expr_elmt -->
 	datatype;variable.
-
 expr_body -->
-	expr_prefix, any_blanks, (expr_elmt ; (expr_elmt, any_blanks, op_infix, any_blanks, expr_body)), any_blanks.
+	expr_prefix, (expr_elmt ; (expr_elmt, expr_op_infix, expr_body)).
 
-blank_coated_expr -->
-	any_blanks, expr, any_blanks.
+prefix_multi -->
+	op_prefix, any_blanks, expr_prefix.
+expr_prefix -->
+	eps; prefix_multi.
+expr_op_infix -->
+	any_blanks, op_infix, any_blanks.
 
-maybe_add_another_expr_body -->
-	eps; (any_blanks, op_infix, blank_coated_expr).
+maybe_expr_after -->
+	eps; (expr_op_infix, expr_body).
+maybe_expr_before -->
+	eps; (expr_body, expr_op_infix).
+
+expr_open_bracket -->
+	"(", any_blanks.
+expr_close_bracket -->
+	any_blanks, ")".
 
 expr -->
-	expr_body;("(", blank_coated_expr, maybe_add_another_expr_body, ")", maybe_add_another_expr_body).
+	((eps; "not",(prefix_multi;blanks)), expr_body);
+	(maybe_expr_before, expr_open_bracket, expr, expr_close_bracket);
+	(expr_open_bracket, expr, expr_close_bracket, maybe_expr_after).
+
 
 assign -->
 	variable, any_blanks, op_assignment, any_blanks, expr.
-
 assign_without_op -->
 	variable, any_blanks, op_assignment_single, any_blanks, expr.
