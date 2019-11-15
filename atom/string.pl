3a91170be63f20d6a6b1898cdb77ce28956d2ae9
William <williamong1400@gmail.com>
2019-11-15 22:59:19 +0700

    piiiiiiiiiiiiii

--- atom/string.pl
+++ atom/string.pl
@@ -1,7 +1,3 @@
-alphanumeric -->
-    (letter,(eps;alphanumeric));
-    (digit,(eps;alphanumeric)).
-
 lowercase -->
     "a";"b";"c";"d";"e";"f";"g";"h";"i";"j";"k";"l";"m";"n";"o";"p";"q";"r";"s";"t";"u";"v";"w";
     "x";"y";"z".
@@ -28,7 +24,7 @@ single_quote -->
 double_quote -->
     "\"".
 escaped_single_quote -->
-    "\\'"
+    "\\'".
 escaped_double_quote -->
     "\\\"".
 
