fb57de6d00aa4d012b9a9ecdf24fecec310621bb
Jonathan Yudi <jonathanyudigun@gmail.com>
2019-11-18 13:15:24 +0700

    fix inline stmt

--- compound/block.pl
+++ compound/block.pl
@@ -5,20 +5,17 @@ block -->
 		(newline,statement)
 	).
 
-colon -->
-	any_blanks, ":", any_blanks.
-
 statement -->
 	statement_single;(statement_single,statement).
 
 inline_statement -->
-	any_blanks, statement_single.
-
-statement_single -->
-	eof;(any_tabs, (compound_stmt;simple_stmt), newline).
+	any_blanks, statement_elmt.
 
 compound_stmt -->
-	(if;while;for;with;funcdef;classdef).
-
+	if;while;for;with;funcdef;classdef.
 simple_stmt -->
-	(assign;pass;return;break;continue;import;expr).
+	assign;pass;return;break;continue;import;expr.
+statement_elmt -->
+	compound_stmt;simple_stmt.
+statement_single -->
+	eof;(any_tabs, statement_elmt, newline).
--- tesjojo.py
+++ tesjojo.py
@@ -1,2 +1,2 @@
-def a():
-	print(a)
+for i in range(n):
+	if (i%2 == 0) : x += i, a
