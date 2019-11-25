77df5328699ca09ba49226306f39eac2625128b7
Jonathan Yudi <jonathanyudigun@gmail.com>
2019-11-25 23:03:48 +0700

    fix script

--- scripts/script_parsing_kwd.py
+++ scripts/script_parsing_kwd.py
@@ -3,13 +3,17 @@ with open("words.txt", "r") as f:
 hasil = []
 for word in words:
 	for i in range(len(word)):
-		cur = "char_"+word[i:-1]+" -> '"+word[i]+"'"
+		cur = "char_"+word[i:-1]+" -> char_"+word[i]
 		if i != len(word)-2:
 			cur += " char_"+word[i+1:-1]
 		else:
-			hasil.append(cur)
 			break
 		hasil.append(cur)
 with open("hasil.txt", "w") as f:
+	for i in range(26):
+		c = chr(ord("a") + i)
+		print("char_"+ c + " -> '" + c + "'", file = f)
+		c = chr(ord("A") + i)
+		print("char_" + c + " -> '" + c + "'", file = f)
 	for h in hasil:
 		print(h, file = f)
